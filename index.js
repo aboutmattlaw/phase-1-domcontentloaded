d9f11c4ac89166dda23cf51a81621e2545e70aba
Matt <aboutmattlaw@gmail.com>
2021-09-12 10:31:54 -0400

    commit tree 192b3b086214f10280f628d2138c1a587682642c

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded" , cool);
+
+function cool(){
+    console.log("helo")
+    const text = document.querySelector("#text");
+    text.textContent = "This is really cool!"
+}
+
+
